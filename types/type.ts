58a005f7744dcee390bcc9f362df6255d7c02664
SHIROHA <1196768261@qq.com>
2022-03-03 14:15:34 +0800

    #FEAT: 新增类型提醒

--- types/type.ts
+++ types/type.ts
@@ -21,7 +21,49 @@ declare class NaruseComponent {
      * 渲染函数
      */
     render: () => any;
-
 }
 
+
+/**
+ * 自定义广告的引入组件
+ */
+declare interface $adImport {
+    /** 广告内容 */
+    adData: {
+        message: "OK" | "offline",
+        open_id: string,
+        results: [
+            {
+                creative_name: string,
+                dest_url: string,
+                pid: number,
+                img_size: string,
+                secondary_class: string,
+                creative_id: string,
+                primary_class: string,
+                user_define: {
+                    id: "templateDefine",
+                    title: "自定义模板",
+                    body: {
+                        /** 加载的代码 */
+                        code: string;
+                        [key: string]: string;
+                    }
+                },
+                group_id: number,
+                img_path: string,
+                creative_type: string,
+                pid_name: string,
+                plan_id: number,
+                rdomNum: 0,
+            }
+        ],
+        return_num: "1" | "0",
+        status: "200" | "500",
+        total_num: 1 | 0,
+        createTime: number
+    },
+    /** 此方法只会在调取showCustomAdDecorator时才会传入 */
+    callback: (parma: boolean) => void;
+}
 
