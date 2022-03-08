58eb6142ebe3948eea189990af4a17dad9bdc694
SHIROHA <1196768261@qq.com>
2022-03-09 00:28:18 +0800

    #FIX & FEAT: 修复了import错误命名，增加了tree shaking

--- package/naruse-webpack-loader/naruse-webpack-plugin.js
+++ package/naruse-webpack-loader/naruse-webpack-plugin.js
@@ -1,9 +1,26 @@
 const { Compilation } = require('webpack');
 const narusejsLoader = require('./narusejs-loader');
 const ConcatSource = require('webpack-sources').ConcatSource;
+const chalk = require('chalk');
+const readline = require('readline');
+const uglifyJs = require('uglify-js');
 const pluginName = 'NaruseWebpackPlugin';
+const compressOption = {
+    unused: true,
+    toplevel: true,
+    unsafe_undefined: true,
+    typeofs: false,
+}
+const clearConsole = () => {
+    const blank = '\n'.repeat(process.stdout.rows)
+    console.log(blank)
+    readline.cursorTo(process.stdout, 0, 0)
+    readline.clearScreenDown(process.stdout)
+}
 
-
+const clearViod = (string) => {
+    return string.replace(/void 0/g, 'undefined');
+}
 
 function NaruseWebpackPlugin(options) {
     this.options = options
@@ -16,16 +33,27 @@ NaruseWebpackPlugin.prototype.apply = function (compiler) {
                 stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONS, // see below for more stages
             },
             (assets) => {
+                clearConsole();
                 const code = compilation.assets['index.js'].source();
-                assets['index.js'] = new ConcatSource(`export default \`${narusejsLoader(code, { minified: false })}\``)
-                assets['dist.js'] = new ConcatSource(narusejsLoader(code, { minified: true }))
-                console.log(new Date().toLocaleTimeString(), '【naruse-plugin】【生成完毕】');
+                const compiledCode = narusejsLoader(code, { minified: false });
+                const reBuildCode = uglifyJs.minify(compiledCode, {
+                    compress: compressOption,
+                    output: {
+                        beautify: true,
+                    }
+                });
+                assets['index.js'] = new ConcatSource(`export default \`${clearViod(reBuildCode.code)}\``);
+                const minifiedCode = uglifyJs.minify(compiledCode, {
+                    compress: compressOption,
+                }).code;
+                assets['dist.js'] = new ConcatSource(minifiedCode);
+                console.log(chalk.green(new Date().toLocaleTimeString(), '【naruse-plugin】【生成完毕】'));
                 Object.entries(assets).forEach(([pathname, source]) => {
-                    console.log(`— ${pathname}: ${source.size()} bytes`);
+                    console.log(chalk.gray(`— ${pathname}: ${source.size()} bytes`));
                 });
             }
         );
-
     })
+
 }
 module.exports = NaruseWebpackPlugin;
