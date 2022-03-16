6813c87d587138561aa899464469d4b353de056c
高灵易 <zhang4895a@163.com>
2022-03-16 15:29:00 +0800

    #FEAT:更新演示文件

--- src/adverts/index.js
+++ src/adverts/index.js
@@ -1,10 +1,20 @@
-import { getText,tips } from './module';
-console.log('全局属性 设备信息', my.getSystemInfoSync());
-console.log('全局属性 用户信息 userInfo', $userInfoChanger.getUserInfo());
-console.log('全局属性 广告信息 adData', $adImport.adData);
+import { getText, tips } from './module';
+import { isBlueBirdUser, getBlueBirdUserExpireTime, isIOS } from '@utils/userInfo';
+console.log('全局属性================================================');
+console.log('设备信息', my.getSystemInfoSync());
+console.log('用户信息 userInfo', $userInfoChanger.getUserInfo());
+console.log('广告信息 adData', $adImport.adData);
+
+console.log('工具测试================================================');
+console.log('设备是否是ios ', isIOS());
+console.log('是否为青鸟用户 ', isBlueBirdUser());
+console.log('青鸟用户的到期时间距离现在多久 ', getBlueBirdUserExpireTime());
+
+console.log('语法支持================================================');
 const obj1 = { a: 1 };
 const obj2 = { b: 1 };
 console.log('拓展运算', { ...obj1, ...obj2 });
+console.log('模板字符串', `你好${true}`);
 
 class Demo extends NaruseComponent {
     constructor () {
