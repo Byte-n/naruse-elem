ff7328d1ac4de9f6e864f89de4fcd1e1032473c8
wtq <993832314@qq.com>
2022-03-29 14:52:16 +0800

    #FIX: 续费模板优化

--- src/adverts/adOrderTemplate/index.js
+++ src/adverts/adOrderTemplate/index.js
@@ -1,26 +1,29 @@
+import { buryAdPageView } from '@utils/beacon';
+
 const templateMainStyle = { width: '750rpx', height: '100%' };
-const firstTemStyle = { width: '750rpx', height: '750rpx', display: 'flex', justifyContent: 'center', alignItems: 'center' };
+const firstTemStyle = { width: '750rpx', height: '750rpx', background: '#FFF', display: 'flex', justifyContent: 'center', alignItems: 'center' };
 const firstTemImgStyle = { width: '700rpx', height: '700rpx' };
-const secondTemStyle = { marginTop: '10rpx', position: 'relative' };
+const secondTemStyle = { position: 'relative' };
 const secondTemImgStyle = { width: '750rpx', height: '2025rpx' };
-const orderBtnStyle = { width: '750rpx', height: '124rpx', position: 'absolute', top: '100rpx' };
+const orderBtnStyle = { width: '750rpx', height: '124rpx', position: 'absolute', top: '20rpx' };
 const iosBtnStyle = { width: '700rpx', height: '124rpx' };
 const androidBtnStyle = { display: 'flex', justifyContent: 'space-around' };
 const androidYearBtnStyle = { width: '340rpx', height: '124rpx' };
 const androidQuarterBtnStyle = { width: '340rpx', height: '100rpx', marginTop: '24rpx' };
 
 // 用户信息
-const userInfo = $userInfoChanger.getUserInfo()
+const userInfo = $userInfoChanger.getUserInfo();
 // 广告信息
 const adInfo = $adImport.adData.results[0];
 // 订购地址
 const orderQuarterLink = 'https://fuwu.taobao.com/ser/confirmOrder1.htm?commonParams=activityCode%3AACT_877021141_220301155305%3BagentId%3Afuwu.taobao.com%7Cmarketing-Order-0%3BmarketKey%3AFWSPP_MARKETING_URL%3BpromIds%3A%5B1007975540%5D&subParams=cycleNum%3A3%2CcycleUnit%3A2%2CitemCode%3AFW_GOODS-1827490-v2&sign=14A525439F81BBA0EA8C790607D732F9&spm=a313p.266.ei5lud.1373001755278&short_name=Y4.7rQrL&app=chrome';
 const orderYearLink = 'https://fuwu.taobao.com/ser/confirmOrder1.htm?commonParams=activityCode%3AACT_877021141_220301155305%3BagentId%3Afuwu.taobao.com%7Cmarketing-Order-0%3BmarketKey%3AFWSPP_MARKETING_URL%3BpromIds%3A%5B1007975540%5D&subParams=cycleNum%3A12%2CcycleUnit%3A2%2CitemCode%3AFW_GOODS-1827490-v2&sign=A1D51A1D1B2DEC8C558E442A5036FE99&spm=a313p.266.ei5lud.1371656866520&short_name=Y4.7kyWZ&app=chrome';
 // 图片地址
-const mainBackgroundImg = 'http://q.aiyongtech.com/ad/images/bmV3X3RlbXBsYXRl_1646981993571.png';
+// const mainBackgroundImg = 'http://q.aiyongtech.com/ad/images/bmV3X3RlbXBsYXRl_1646981993571.png';
 const iosOrderBtn = 'http://q.aiyongtech.com/ad/images/aW9zX2p1bXBfc2VydmljZV9idG4=_1646982081811.gif';
 const ardroidOrderYearBtn = 'http://q.aiyongtech.com/ad/images/b25lX3F1YXJ0ZXJfcHJpY2VfYnRu_1646982094639.png';
 const ardroidOrderQuarterBtn = 'http://q.aiyongtech.com/ad/images/b25lX3llYXJfcHJpY2VfYnRu_1646982097169.gif';
+const mainBackgroundImg = 'http://q.aiyongtech.com/ad/images/bEFMUEp3WTdSMlROaW1uTkI2UE5BdTRfNzUwXzE5NTU=_1647512219465.png';
 
 console.log('ad_ adInfo', adInfo);
 console.log('ad_ userInfo', userInfo);
@@ -37,7 +40,8 @@ class AdOrderTemplate extends NaruseComponent {
         };
     }
     
-    componentDidMount() {
+    componentDidMount () {
+        buryAdPageView();
         this.setState({ isIOS: this.isIOS() });
     }
 
@@ -61,7 +65,7 @@ class AdOrderTemplate extends NaruseComponent {
             console.log('ad_jumplink====');
             return function () {
                 console.log('ad_url====', url);
-                my.qn.navigateToWebPage({ url: url });
+                my.qn.navigateToWebPage({ url });
                 console.log('adBeacon1111');
                 $adSensorsBeacon.adOrderNowBeacon(adInfo, url == orderQuarterLink ? '52/季度' : '148/年', '');
             }
