aa065c266b0d3771f66b6771dc1a291a85aaa6ca
wtq <993832314@qq.com>
2022-04-02 08:26:49 +0800

    #FIX: 增加模板关闭

--- src/adverts/tradePcOrderTem/index.js
+++ src/adverts/tradePcOrderTem/index.js
@@ -2,12 +2,22 @@ import { Component, navigateToWebPage } from 'Naruse';
 import { buryAdOrderNow, buryAdPageView } from '@utils/beacon';
 
 // 模板样式
-const tradePcOrderTem = { width: '100%', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' };
-const tradePcOrderTemMain = { width: '800px', height: '500px' };
-const topPart = { width: '800px', height: '390px' };
-const bottomPart = { width: '800px', height: '110px', position: 'relative' };
-const bottomImg = { width: '800px', height: '110px' };
-const bottomClickPart = { width: '400px', height: '110px', position: 'absolute', top: '0' };
+const tradePcContainer = { width: '100vw', height: '100vh', position: 'fixed', top: '0', left: '0', backgroundColor: 'rgba(0, 0, 0, 0.7)', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 900 };
+const tradePcOrderTem = { width: '800rpx', height: '506rpx', background: '#FFF', display: 'flex', alignItems: 'center', justifyContent: 'center' };
+const tradePcOrderTemMain = { width: '800rpx', height: '500rpx', position: 'relative' };
+const topPart = { width: '800rpx', height: '390rpx' };
+const bottomPart = { width: '800rpx', height: '110rpx', position: 'relative' };
+const bottomImg = { width: '800rpx', height: '110rpx' };
+const bottomClickPart = { width: '400rpx', height: '110rpx', position: 'absolute', top: '0' };
+const closeStyle = {
+    color: 'rgba(255, 255, 255, 0.8)',
+    padding: '10rpx 40rpx',
+    border: '2rpx solid',
+    borderRadius: '50rpx',
+    position: 'absolute',
+    bottom: '-100rpx',
+    left: '230px',
+};
 
 // 用户信息
 const userInfo = $userInfoChanger.getUserInfo();
@@ -25,7 +35,11 @@ const {
 } = adInfo.user_define.body;
 
 export default class TradePcOrderTem extends Component {
-    componentDidMount () {
+    constructor() {
+        this.state = { isShow: true };
+    }
+
+    componentDidMount() {
         buryAdPageView();
     }
 
@@ -36,17 +50,32 @@ export default class TradePcOrderTem extends Component {
         navigateToWebPage({ url });
     }
 
-    render () {
+    colsePopup = () => {
+        $adImport.uninstall();
+        this.setState({ isShow: false });
+    }
+
+    render() {
+        const { isShow } = this.state;
         return (
-            <view style={tradePcOrderTem}>
-                <view style={tradePcOrderTemMain}>
-                    <image style={topPart} src={topImgUrl} />
-                    <view style={bottomPart}>
-                        <image style={bottomImg} src={bottomImgUrl} />
-                        <view style={{ ...bottomClickPart, left: '0' }} onClick={() => this.jumpOrderLink('quarter')}></view>
-                        <view style={{ ...bottomClickPart, right: '0' }} onClick={() => this.jumpOrderLink('year')}></view>
-                    </view>
-                </view>
+            <view>
+                {
+                    isShow && (
+                        <view style={tradePcContainer} onClick={this.colsePopup}>
+                            <view style={tradePcOrderTem}>
+                                <view style={tradePcOrderTemMain}>
+                                    <image style={topPart} src={topImgUrl} />
+                                    <view style={bottomPart}>
+                                        <image style={bottomImg} src={bottomImgUrl} />
+                                        <view style={{ ...bottomClickPart, left: '0' }} onClick={() => this.jumpOrderLink('quarter')}></view>
+                                        <view style={{ ...bottomClickPart, right: '0' }} onClick={() => this.jumpOrderLink('year')}></view>
+                                    </view>
+                                    <text style={closeStyle} onClick={this.colsePopup}>{'关闭'}</text>
+                                </view>
+                            </view>
+                        </view>
+                    )
+                }
             </view>
         )
     }
