915c4bcd07c3b9dda0019cf11f0d3b58774778c2
wtq <993832314@qq.com>
2022-04-06 14:32:14 +0800

    #FIX: 功能点模板去除白底

--- src/adverts/tradePcOrderTem/index.js
+++ src/adverts/tradePcOrderTem/index.js
@@ -4,22 +4,23 @@ import { buryAdOrderNow, buryAdPageView } from '@utils/beacon';
 
 // 模板样式
 const tradePcContainer = { width: '100vw', height: '100vh', position: 'fixed', top: '0', left: '0', backgroundColor: 'rgba(0, 0, 0, 0.7)', display: 'flex', alignItems: 'center', justifyContent: 'center', zIndex: 900 };
-const tradePcOrderTem = { width: '800rpx', height: '500rpx', background: '#FFF', display: 'flex', alignItems: 'center', justifyContent: 'center' };
+const tradePcOrderTem = { width: '800px', height: '500px', display: 'flex', alignItems: 'center', justifyContent: 'center' };
 const dialogBox = { transition: 'all 0.5s', opacity: '1', transform: 'translateY(0)' };
 const dialogBoxDown = { opacity: '0', transform: 'translateY(-50%)' };
-const tradePcOrderTemMain = { width: '800rpx', height: '500rpx', position: 'relative' };
-const topPart = { width: '800rpx', height: '390rpx' };
-const bottomPart = { width: '800rpx', height: '100rpx', position: 'relative' };
-const bottomImg = { width: '800rpx', height: '100rpx' };
-const bottomClickPart = { width: '400rpx', height: '100rpx', position: 'absolute', top: '0' };
+const tradePcOrderTemMain = { width: '800px', height: '500px', position: 'relative' };
+const topPart = { width: '800px', height: '390px' };
+const bottomPart = { width: '800px', height: '100px', position: 'relative', cursor: 'pointer', marginTop: '-4px' };
+const bottomImg = { width: '800px', height: '100px' };
+const bottomClickPart = { width: '400px', height: '100px', position: 'absolute', top: '0' };
 const closeStyle = {
     color: 'rgba(255, 255, 255, 0.8)',
-    padding: '10rpx 40rpx',
-    border: '2rpx solid',
-    borderRadius: '50rpx',
+    padding: '10px 40px',
+    border: '2px solid #FFF',
+    borderRadius: '50px',
     position: 'absolute',
-    bottom: '-100rpx',
-    left: '340rpx',
+    bottom: '-100px',
+    left: '340px',
+    cursor: 'pointer',
 };
 
 // 用户信息
@@ -51,7 +52,8 @@ export default class TradePcOrderTem extends Component {
     }
 
     componentDidMount() {
-        buryAdPageView();
+        // 发送初始化埋点
+        $adSensorsBeacon.adViewBeacon(adInfo, false, adInfo.pid);
         new Promise((res) => {
             setTimeout(res, 500);
         }).then(() => {
@@ -75,13 +77,13 @@ export default class TradePcOrderTem extends Component {
      * 模板弹窗关闭
      */
     colsePopup = () => {
-        $uninstall();
         this.setState({ animation: true });
         new Promise((res) => {
             setTimeout(res, 500);
         }).then(() => {
             this.setState({ isShow: false });
         });
+        $uninstall();
     }
 
     render() {
@@ -102,7 +104,12 @@ export default class TradePcOrderTem extends Component {
                                         <view style={{ ...bottomClickPart, left: '0' }} onClick={() => this.jumpOrderLink('quarter')}></view>
                                         <view style={{ ...bottomClickPart, right: '0' }} onClick={() => this.jumpOrderLink('year')}></view>
                                     </view>
-                                    <text style={closeStyle} onClick={this.colsePopup}>{'关闭'}</text>
+                                    <view
+                                        style={closeStyle}
+                                        onClick={this.colsePopup}
+                                    >
+                                        {'关闭'}
+                                    </view>
                                 </view>
                             </view>
                         </view>
