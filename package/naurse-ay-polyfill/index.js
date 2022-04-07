0ff1600f8305708949097d72704860f89243368f
SHIROHA <1196768261@qq.com>
2022-04-07 19:11:29 +0800

    #FEAT: 升级eslint 支持 class static proprety

--- package/naurse-ay-polyfill/index.js
+++ package/naurse-ay-polyfill/index.js
@@ -10,8 +10,8 @@ const adData = {
     open_id: 'e000b529-879b-4bc2-8ab0-a33c829ee65d',
     results: [
         {
-            creative_name: '测试创意-促销水印-功能点-Android-customAd',
-            dest_url: 'https://fuwu.taobao.com/ser/confirmOrder1.htm?commonParams=activityCode%3AACT_877021141_220222162004%3BagentId%3Afuwu.taobao.com%7Cmarketing-Order-0%3BmarketKey%3AFWSPP_MARKETING_URL%3BpromIds%3A%5B1007951687%5D&subParams=cycleNum%3A12%2CcycleUnit%3A2%2CitemCode%3AFW_GOODS-1827490-v3&sign=AD247593B83EF4EA347728425E6B9C85&spm=a313p.266.ei5lud.1370863636957&short_name=Y4.77tvN&app=chrome',
+            creative_name: '测试创意',
+            dest_url: '未知',
             pid: 5245,
             img_size: '600*720',
             secondary_class: '',
@@ -25,7 +25,7 @@ const adData = {
             group_id: 66685610,
             img_path: 'http://q.aiyongtech.com/ad/images/55S75p2/5aSH5Lu9MTM=_1646620823153.png',
             creative_type: '4',
-            pid_name: 'FMB商品初级版-促销水印-单个水印点击',
+            pid_name: '测试pid',
             plan_id: 15967,
             rdomNum: 0,
             open_id: 'e000b529-879b-4bc2-8ab0-a33c829ee65d',
@@ -34,27 +34,6 @@ const adData = {
     return_num: '1',
     status: '200',
     total_num: 1,
-    result: {
-        creative_name: '测试创意-促销水印-功能点-Android-customAd',
-        dest_url: 'https://fuwu.taobao.com/ser/confirmOrder1.htm?commonParams=activityCode%3AACT_877021141_220222162004%3BagentId%3Afuwu.taobao.com%7Cmarketing-Order-0%3BmarketKey%3AFWSPP_MARKETING_URL%3BpromIds%3A%5B1007951687%5D&subParams=cycleNum%3A12%2CcycleUnit%3A2%2CitemCode%3AFW_GOODS-1827490-v3&sign=AD247593B83EF4EA347728425E6B9C85&spm=a313p.266.ei5lud.1370863636957&short_name=Y4.77tvN&app=chrome',
-        pid: 5207,
-        img_size: '600*720',
-        secondary_class: '',
-        creative_id: '43330',
-        primary_class: '',
-        user_define: {
-            id: 'templateDefine',
-            title: '自定义模板',
-            body: advertUserDefine,
-        },
-        group_id: 66685610,
-        img_path: 'http://q.aiyongtech.com/ad/images/55S75p2/5aSH5Lu9MTM=_1646620823153.png',
-        creative_type: '4',
-        pid_name: 'FMB商品初级版-促销水印-单个水印点击',
-        plan_id: 15967,
-        rdomNum: 0,
-        open_id: 'e000b529-879b-4bc2-8ab0-a33c829ee65d',
-    },
     createTime: 1646813903418,
 };
 
