5dccd1bff095f2a6eee52d108e395a34a8fd9081
SHIROHA <1196768261@qq.com>
2022-04-15 18:59:25 +0800

    #CHORRE: 修复了eslint不支持jsx的问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,14 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
         ecmaFeatures: { jsx: true },
-        babelOptions: { rootMode: 'upward' },
+        babelOptions: {
+            rootMode: 'upward',
+            plugins: [
+                [
+                    '@babel/plugin-transform-react-jsx',
+                ],
+            ],
+        },
     },
     parser: '@babel/eslint-parser',
     extends: [
@@ -12,33 +19,31 @@ module.exports = {
         'eslint-config-ay/import',
     ],
     rules: { 'linebreak-style': 'off' },
-    overrides: [
-        {
-            files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
-            rules: {
-                'no-console': 'off',
-                'prefer-destructuring': ['error', {
-                    array: false,
-                    object: true,
-                }],
-                'constructor-super': 'off',
-                'no-this-before-super': 'off',
-                'id-length': 'off',
-                'require-jsdoc-except/require-jsdoc': 0,
-                'no-cond-assign': 0,
-                'eol-last': 'off',
-            },
-            globals: {
-                $adImport: 'readonly',
-                $mappUtils: 'readonly',
-                my: 'readonly',
-                $moment: 'readonly',
-                $adSensorsBeacon: 'readonly',
-                $userInfoChanger: 'readonly',
-                $openChat: 'readonly',
-                $ayApi: 'readonly',
-                Promise: 'readonly',
-            },
+    overrides: [{
+        files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+        rules: {
+            'no-console': 'off',
+            'prefer-destructuring': ['error', {
+                array: false,
+                object: true,
+            }],
+            'constructor-super': 'off',
+            'no-this-before-super': 'off',
+            'id-length': 'off',
+            'require-jsdoc-except/require-jsdoc': 0,
+            'no-cond-assign': 0,
+            'eol-last': 'off',
         },
-    ],
+        globals: {
+            $adImport: 'readonly',
+            $mappUtils: 'readonly',
+            my: 'readonly',
+            $moment: 'readonly',
+            $adSensorsBeacon: 'readonly',
+            $userInfoChanger: 'readonly',
+            $openChat: 'readonly',
+            $ayApi: 'readonly',
+            Promise: 'readonly',
+        },
+    }],
 };
