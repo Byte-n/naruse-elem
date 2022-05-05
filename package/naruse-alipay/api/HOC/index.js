47596a713ef434b21e6b62092857f0dab95072ad
SHIROHA <1196768261@qq.com>
2022-05-05 17:38:32 +0800

    #FEAT: 新增withPage,renderComponentOnPage的api

---
+++ package/naruse-alipay/api/HOC/index.js
@@ -0,0 +1,18 @@
+import { createElement } from '../../core/createElement';
+import { NaruseComponent } from '../../core/component';
+import { getPageInstance } from '../../core/page';
+export const withPage = (component) => {
+    return class extends NaruseComponent {
+        render () {
+            const page = getPageInstance(this.$updater?.component);
+            const currentPage = {
+                route: page.route,
+                events: {
+                    on: page.on.bind(page),
+                    off: page.off.bind(page),
+                },
+            };
+            return createElement(component, { ...this.props, currentPage });
+        }
+    };
+};
