824a349e4756765a9f3126fd8f3413af84513588
SHIROHA <1196768261@qq.com>
2022-06-20 14:10:35 +0800

    #FIX: 修复小程序中children中与React不一致的问题

--- package/naruse-alipay/core/index.js
+++ package/naruse-alipay/core/index.js
@@ -1,9 +1,11 @@
 
-import { renderComponentOnPageWithCode } from "../expand/index";
+import { renderComponentOnPage, renderComponentOnPageWithCode } from "../expand/index";
 import { naruseInit } from "./init";
 import { createMainBehavior } from "./mainComponent";
 import { Naruse, naruseExtend } from "./naurse";
 import { createSubBehavior } from "./subComponent";
+
+naruseExtend({ renderComponentOnPage });
 
 export {
     createMainBehavior,
