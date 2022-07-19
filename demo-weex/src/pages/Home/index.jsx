b3fbd8f2db890021c02847f9717c365e70ab72ef
SHIROHA <1196768261@qq.com>
2022-07-19 16:16:52 +0800

    #FEAT: 更新demo-weex使用例子

--- demo-weex/src/pages/Home/index.jsx
+++ demo-weex/src/pages/Home/index.jsx
@@ -1,18 +1,17 @@
 import { createElement } from 'rax';
 import View from 'rax-view';
 import Text from 'rax-text';
-
 import styles from './index.module.css';
-
 import Logo from '@/components/Logo';
+import { createElement as naruseCreateElement } from '../../../../package/naruse-weex/build/lib'
 
 export default function Home() {
   return (
     <View className={styles.homeContainer}>
-      <Logo uri="//gw.alicdn.com/tfs/TB1MRC_cvb2gK0jSZK9XXaEgFXa-1701-1535.png" />
       <Text className={styles.homeTitle}>Welcome to Your Rax App</Text>
-      <Text className={styles.homeInfo}>More information about Rax</Text>
-      <Text className={styles.homeInfo}>Visit https://rax.js.org</Text>
+      {naruseCreateElement('text', { style: { height: '200rpx', backgroundColor: 'red', fontSize: '50rpx' }, selectable: true, onClick: () => {
+        console.log(123);
+      }  }, '123')}
     </View>
   );
 }
