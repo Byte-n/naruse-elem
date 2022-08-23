b5a5b618555d51cf0b8091e35312c680715e1b56
SHIROHA <1196768261@qq.com>
2022-08-23 17:29:41 +0800

    #FEAT: 【naruse-parser】编译器换为acorn 0.12.0 解释器切换为全TS 支持完整的ES5 与部分es6 补充大量测试用例

--- package/naruse-parser/test.js
+++ package/naruse-parser/test.js
@@ -1,11 +1,13 @@
-import { run } from './index.js/index.js';
+import { run } from './index.js';
+
 
 
 
 const code = `
-function ww (r) { console.log(123); r&&r(); };
-function qq () { return 'then'; }
-new Promise(ww)[qq()](ww);
+
+console.log("".concat("123"))
 `
-console.log(run(code));
+
+
+console.log(run(code, { $$import: (a) => a }));
 
