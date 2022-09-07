b19fe368fc67f1d09c716d9bc07dba0413e515a7
SHIROHA <1196768261@qq.com>
2022-09-07 16:54:09 +0800

    #FEAT: 【naruse-alipay】切换为ts

--- package/naruse-alipay/rollup.config.js
+++ package/naruse-alipay/rollup.config.js
@@ -2,7 +2,7 @@ const alias = require('@rollup/plugin-alias');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const replace = require('@rollup/plugin-replace');
 const { uglify } = require('rollup-plugin-uglify');
-const babel = require('rollup-plugin-babel');
+const typescript = require('@rollup/plugin-typescript');
 
 const customResolver = nodeResolve({ extensions: ['.mjs', '.js', '.jsx', '.json', '.sass', '.scss', '.css'] });
 
@@ -10,18 +10,18 @@ const customResolver = nodeResolve({ extensions: ['.mjs', '.js', '.jsx', '.json'
  * @type {import('rollup').RollupOptions}
  */
 module.exports = {
-    input: './core/index.js',
+    input: './core/index.ts',
     output: {
         file: './build/lib.js',
         format: 'es',
     },
     plugins: [
         alias({ customResolver }),
-        replace({
-            __IS_ALIAPY__: true,
-            __IS_H5__: false,
-        }),
-        babel({ exclude: 'node_modules/**' }),
-        uglify({ mangle: {  toplevel: true}, compress: { toplevel: true } })
+        typescript(),
+        // replace({
+        //     __IS_ALIAPY__: true,
+        //     __IS_H5__: false,
+        // }),
+        // uglify({ mangle: {  toplevel: true}, compress: { toplevel: true } })
     ],
 };
