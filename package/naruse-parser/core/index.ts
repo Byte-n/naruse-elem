67f840f7475b5d1de11a87cded6e47c4ad99a652
SHIROHA <1196768261@qq.com>
2022-12-01 18:29:46 +0800

    #FEAT: naruse-parser 开源迁移

--- package/naruse-parser/core/index.ts
+++
@@ -1,7 +0,0 @@
-import { Runner } from './run';
-
-export const run = (code: string, injectObject: any) => {
-    const runner = new Runner();
-    return runner.run(code, injectObject);
-}
-export default run;
