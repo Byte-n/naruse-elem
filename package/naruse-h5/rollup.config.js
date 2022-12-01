d5fc54147c8bcd1000aade42111f382de3c92c22
SHIROHA <1196768261@qq.com>
2022-12-01 19:19:11 +0800

    #FEAT: 【h5】升级naruse-parser依赖为0.0.8

--- package/naruse-h5/rollup.config.js
+++ package/naruse-h5/rollup.config.js
@@ -1,16 +1,16 @@
-const alias = require('@rollup/plugin-alias');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const externalGlobals = require('rollup-plugin-external-globals');
 const css = require('../rollup-plugin-naruse/plugin/naruse-css-loader');
 const inject = require('@rollup/plugin-inject');
 const typescript = require('@rollup/plugin-typescript');
 const replace = require('@rollup/plugin-replace');
+const commonjs = require('@rollup/plugin-commonjs');
 
 const fs = require('fs');
 const path = require('path');
 const version = JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'), 'utf-8')).version;
 
-const customResolver = nodeResolve({ extensions: ['.mjs', '.js', '.jsx', '.json', '.sass', '.scss', '.css'] });
+const customResolver = nodeResolve({ extensions: ['.mjs', 'cjs', '.js', '.jsx', '.json', '.sass', '.scss', '.css'] });
 
 const isDev = process.env.BUILD === 'development';
 
@@ -20,14 +20,15 @@ const config = {
         file: './dist/index.js',
         format: 'es',
     },
-    external: [ 'react' ],
+    external: ['react'],
     plugins: [
-        replace({ __VERSION__: JSON.stringify(version) }),
-        alias({ customResolver }),
-        externalGlobals({ react: 'React' }),
-        inject({ React: 'react' }),
-        typescript(),
         css(),
+        typescript(),
+        customResolver,
+        commonjs(),
+        externalGlobals({ react: 'React' }),
+        replace({ __VERSION__: JSON.stringify(version) }),
+        inject({ React: 'react' }),
     ],
 };
 
