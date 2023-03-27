85ddfe4e35eb72cdc0b8da30a5fe5899aa117472
SHIROHA <1196768261@qq.com>
2023-03-27 11:51:27 +0800

    #FEAT:【weex】支持 Hooks 与 函数化组件

---
+++ package/naruse-weex/src/core/hooks.ts
@@ -0,0 +1,13 @@
+import { useCallback, useContext, useEffect, useImperativeHandle, useLayoutEffect, useMemo, useReducer, useRef, useState, createContext } from 'rax';
+export const Hooks = {
+    useCallback,
+    useContext,
+    useEffect,
+    useImperativeHandle,
+    useLayoutEffect,
+    useMemo,
+    useReducer,
+    useRef,
+    useState,
+    createContext
+}
