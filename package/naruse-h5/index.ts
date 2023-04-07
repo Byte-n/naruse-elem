de9b0c9c46f2d786f7628ff4f7d13caf2789b9c5
byte <dancingbytes@qq.com>
2023-04-07 14:25:17 +0800

    #FEAT:naruse-h5新增naruseExtend

--- package/naruse-h5/index.ts
+++ package/naruse-h5/index.ts
@@ -1,3 +1,3 @@
-export { Naruse } from './core/jsEngineEnv';
+export { Naruse, naruseExtend } from './core/jsEngineEnv';
 export { naruseInit } from './core/init'
 export { Container as default, Container } from './core/container';
