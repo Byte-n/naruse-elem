6737ff88e38379ee4c67c51a23cca9a7f3408b9b
byte <dancingbytes@qq.com>
2023-04-21 10:17:47 +0800

    #FEAT:naruse-h5引入插件、日志插件

--- package/naruse-h5/index.ts
+++ package/naruse-h5/index.ts
@@ -1,3 +1,13 @@
 export { Naruse, naruseExtend } from './core/jsEngineEnv';
 export { naruseInit } from './core/init'
 export { Container as default, Container } from './core/container';
+import { getNaruseConfig } from "./core/init";
+import { Plugin, registerPlugin as _registerPlugin, LoggerLevel, LoggerLanding, LoggerPlus, LoggerPlugin } from '../naruse-plugin';
+
+const registerPlugin = (name: string, plugin: typeof Plugin, ...params: any []) => {
+    const config = getNaruseConfig();
+    return _registerPlugin(name, plugin, { config }, ...params)
+}
+export {
+    registerPlugin, Plugin, LoggerLevel, LoggerLanding, LoggerPlus, LoggerPlugin
+}
