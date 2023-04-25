46d952585ddb0c70d0c7cb32d0048de89aa503de
byte <dancingbytes@qq.com>
2023-04-25 19:24:46 +0800

    #FEAT:weex 增加插件系统 和 日志插件

--- package/naruse-weex/src/index.ts
+++ package/naruse-weex/src/index.ts
@@ -2,10 +2,20 @@ import './polyfill/flat'
 import Naruse, { naruseExtend } from './core/naruse';
 import { naruseInit } from './core/init';
 import { Container } from './core/container';
+import { getNaruseConfig } from "./core/init";
+import { Plugin, registerPlugin as _registerPlugin, LoggerLevel, LoggerLanding, LoggerPlus, LoggerPlugin } from '../../naruse-plugin';
 
 export {
     Naruse,
     naruseExtend,
     naruseInit,
     Container,
+}
+
+const registerPlugin = (name: string, plugin: typeof Plugin, ...params: any []) => {
+    const config = getNaruseConfig();
+    return _registerPlugin(name, plugin, { config }, ...params)
+}
+export {
+    registerPlugin, Plugin, LoggerLevel, LoggerLanding, LoggerPlus, LoggerPlugin
 }
