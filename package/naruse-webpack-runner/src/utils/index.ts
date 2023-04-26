04d06e891fa59a99f023650e130dd7cd211539f7
SHIROHA <1196768261@qq.com>
2023-04-26 14:22:57 +0800

    #FEAT: 升级 naruse-webpack-runner

--- package/naruse-webpack-runner/src/utils/index.ts
+++ package/naruse-webpack-runner/src/utils/index.ts
@@ -1,1 +1,13 @@
 export const baseExts = ['.js', '.jsx', '.ts', '.tsx', '.mjs'];
+
+
+/**
+ * 深层合并对象
+ */
+export function deepMergeObject(firstObject, secondObject) {
+    for (var key in secondObject) {
+        firstObject[key] = firstObject[key] && Object.prototype.toString.call(firstObject[key]) === "[object Object]" ?
+            deepMergeObject(firstObject[key], secondObject[key]) : secondObject[key];
+    }
+    return firstObject;
+}
