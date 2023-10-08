61b9457ef6d6ba0f96daa249050bc25e05bc5187
SHIROHA <shir0ha@qq.com>
2023-10-08 19:26:46 +0800

    #FEAT: [h5] 支持 appear 相关 API

--- demo/demo-h5/advert/index.tsx
+++ demo/demo-h5/advert/index.tsx
@@ -1,15 +1,40 @@
-import { Component, useState, useEffect, useRef } from 'Naruse';
+import { Component } from 'Naruse';
 
 
-export default function Qwer() {
-    const [first, setfirst] = useState('123')
-    const ww = useRef(123);
-    useEffect(() => {
-        console.log(ww);
-        setTimeout(() => {
-            setfirst(321);
-        }, 2000);
-    }, [])
+export default class Qwer extends Component {
 
-    return <view>123{first}</view>;
+    async componentDidMount() {
+        const qq = await import('./qwer.css');
+        console.log('ccc', qq.ccc);
+    }
+
+    render() {
+        return <view
+            style={{
+                backgroundColor: 'red',
+                width: '100px',
+                height: '10000px',
+            }}>
+            {new Array(100).fill(1).map((item, index) => {
+                return <view
+                    style={{
+                        backgroundColor: 'blue',
+                        width: '100px',
+                        height: '100px',
+                    }}
+                    onAppear={() => {
+                        console.log('onAppear', index);
+                    }}
+                    onDisappear={() => {
+                        console.log('onDisappear', index);
+                    }}
+                    onClick={() => {
+                        console.log('onClick', index);
+                    }}
+                >
+                    {index}
+                </view>;
+            })}
+        </view>;
+    }
 }
