f54fe9880e4d47a87ca2dec1a9546792f468a286
byte-n <dancingbytes@qq.com>
2024-08-02 17:43:27 +0800

    #FEAT: 支付宝小程序支持radio、radio-group

--- demo/demo-alipay/advert/index.tsx
+++ demo/demo-alipay/advert/index.tsx
@@ -4,36 +4,63 @@ import { Component } from 'Naruse';
 export default class Qwer extends Component {
 
     async componentDidMount() {
-        const qq = await import('./qwer.css');
-        console.log('ccc', qq.ccc);
+        // const qq = await import('./qwer.css');
+        // console.log('ccc', qq.ccc);
     }
 
     render() {
-        console.log('render', this);
-        return <view
-        style={{
-            backgroundColor: 'red',
-            width: '100px',
-            height: '10000px',
-         }}>
-            <web-view ref='web' src='https://fuwu.taobao.com/ser/assembleParam.htm?spm=a1z13.2196529.0.0.1b1f519fmbgMhQ&tracelog=search&activityCode=&promIds=&subParams=itemCode:FW_GOODS-1827490-1,cycleNum:12,cycleUnit:2' ></web-view>
-            {new Array(100).fill(1).map((item, index) => {
-                return <view
+        return (
+            <view>
+                {this.renderRadio()}
+                {this.renderOnFirstAppear()}
+            </view>
+        );
+    }
+
+
+    renderOnFirstAppear () {
+        return (
+            <view
                 style={{
-                    backgroundColor: 'blue',
+                    backgroundColor: 'red',
                     width: '100px',
-                    height: '100px',
-                 }}
-                 onFirstAppear={() => {
-                    console.log('onFristAppear', index);
-                }}
-                onClick={() => {
-                    console.log('onClick', index);
-                }}
-                 >
-                    {index}
-                </view>;
+                    height: '10000px',
+                }}>
+                {new Array(100).fill(1).map((item, index) => {
+                    return <view
+                        style={{
+                            backgroundColor: 'blue',
+                            width: '100px',
+                            height: '100px',
+                        }}
+                        onFirstAppear={() => {
+                            console.log('onFristAppear', index);
+                        }}
+                        onClick={() => {
+                            console.log('onClick', index);
+                        }}
+                    >
+                        {index}
+                    </view>;
                 })}
-        </view>;
+            </view>
+        );
+    }
+
+    renderRadio () {
+        const { radioValue = 'b' } = this.state;
+        return (
+            <view>
+                当前选项值：{radioValue}
+                <radio-group onChange={evl => this.setState({ radioValue: evl.detail.value })}>
+                    <radio value={'a'} checked={radioValue === 'a'}>
+                        a选项
+                    </radio>
+                    <radio value={'b'} checked={radioValue === 'b'}>
+                        b选项
+                    </radio>
+                </radio-group>
+            </view>
+        );
     }
 }
