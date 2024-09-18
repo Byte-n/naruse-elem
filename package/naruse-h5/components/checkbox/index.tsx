0577fe39302e565351eb4ecd708d2b4ddb591d87
ccyx <702609592@qq.com>
2024-09-18 20:20:31 +0800

    #FEAT: 优化checkbox组件

--- package/naruse-h5/components/checkbox/index.tsx
+++ package/naruse-h5/components/checkbox/index.tsx
@@ -1,33 +1,61 @@
 import React,{ Component } from 'react';
-import { getPropsDataSet } from '../../utils';
+import { getBaseProps, getPropsDataSet } from '../../utils';
+import { commonEventHander } from 'core/event';
+
+interface CheckboxProps {
+    /** id */
+    id?: string | number;
+    /** 是否选中 */
+    checked?: boolean;
+    /** 名称 */
+    name?: string;
+    /** 颜色 */
+    color?: string;
+    /** 值 */
+    value: string | number;
+    /** 标签 */
+    label: string | number;
+    /** 子元素 */
+    children?: any;
+    /** 是否禁用 */
+    disabled?: boolean;
+    /** 改变事件 */
+    onChange?: Function;
+}
+
+interface CheckboxStates {
+
+}
 
 const h = React.createElement;
-class Checkbox extends Component {
-    /** 改变事件 */
-    handleChange (e) {
-        e.stopPropagation();
-        this.props.onChange && this.props.onChange({ value: this.value });
-    }
+class Checkbox extends Component<CheckboxProps, CheckboxStates> {
+    // /** 改变事件 */
+    // handleChange (e) {
+    //     const { onChange } = this.props;
+    //     e.stopPropagation();
+    //     onChange && onChange({ value: this.value });
+    // }
+    onChange = commonEventHander.bind(this);
+    setRef = (ref: null) => this.ref = ref;
 
     render () {
-        const { checked, name, color, value, disabled, ...nativeProps } = this.props;
+        const { id, checked, name, color, value, label, disabled, children, ...nativeProps } = this.props;
 
         return (
-            <input
-                ref={dom => {
-                    if (!dom) return;
-                    this.inputEl = dom;
-                    if (this.id) dom.setAttribute('id', this.id);
-                }}
-                type='checkbox'
-                value={value}
-                name={name}
-                style={{ color }}
-                checked={checked}
-                disabled={disabled}
-                onChange={this.handleChange.bind(this)}
-                {...getPropsDataSet(nativeProps)}
-            />
+            <label htmlFor={id}
+                {...getBaseProps(this.props, 'label')}>
+                <input
+                    ref={this.setRef}
+                    type='checkbox'
+                    value={value}
+                    style={{ color }}
+                    checked={checked}
+                    disabled={disabled}
+                    onChange={this.onChange}
+                    {...getPropsDataSet(nativeProps)}
+                />
+                {children ? children : (label ? label : value)}
+            </label>
         );
     }
 }
